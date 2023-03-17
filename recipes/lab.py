fa27426ac45476953df982db532f877c85d126fc
QingruiSun <2902753024@qq.com>
2023-03-17 21:42:57 +0800

    pass test about all included

--- recipes/lab.py
+++ recipes/lab.py
@@ -15,7 +15,14 @@ def make_recipe_book(recipes):
     return a dictionary that maps each compound food item name to a list
     of all the ingredient lists associated with that name.
     """
-    raise NotImplementedError
+    compound_recipes = {}
+    for recipe in recipes:
+        if recipe[0] == "compound":
+            if recipe[1] in compound_recipes:
+                compound_recipes[recipe[1]].append(recipe[2])
+            else:
+                compound_recipes[recipe[1]] = [recipe[2]]
+    return compound_recipes
 
 
 def make_atomic_costs(recipes):
@@ -23,7 +30,27 @@ def make_atomic_costs(recipes):
     Given a recipes list, make and return a dictionary mapping each atomic food item
     name to its cost.
     """
-    raise NotImplementedError
+    atomic_cost = {}
+    for recipe in recipes:
+        if recipe[0] == "atomic":
+            atomic_cost[recipe[1]] = recipe[2]
+    return atomic_cost
+
+
+def lowest_cost_helper(recipe_book, atomic_costs, food_item):
+    if food_item in atomic_costs:
+        return atomic_costs[food_item]
+    recipes = recipe_book[food_item]
+    min_cost = 10000000
+    for recipe in recipes:
+        current_cost = 0
+        for element in recipe:
+            current_cost += (
+                lowest_cost_helper(recipe_book, atomic_costs, element[0]) * element[1]
+            )
+        if current_cost < min_cost:
+            min_cost = current_cost
+    return min_cost
 
 
 def lowest_cost(recipes, food_item):
@@ -31,7 +58,9 @@ def lowest_cost(recipes, food_item):
     Given a recipes list and the name of a food item, return the lowest cost of
     a full recipe for the given food item.
     """
-    raise NotImplementedError
+    recipe_book = make_recipe_book(recipes)
+    atomic_costs = make_atomic_costs(recipes)
+    return lowest_cost_helper(recipe_book, atomic_costs, food_item)
 
 
 def scale_recipe(flat_recipe, n):
