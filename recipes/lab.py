db734af414f3892455fc294e44db2a8817ebb3d3
QingruiSun <2902753024@qq.com>
2023-03-17 23:31:12 +0800

    finished and pass cheapest flat recipe

--- recipes/lab.py
+++ recipes/lab.py
@@ -90,7 +90,10 @@ def scale_recipe(flat_recipe, n):
     Given a dictionary of ingredients mapped to quantities needed, returns a
     new dictionary with the quantities scaled by n.
     """
-    raise NotImplementedError
+    new_recipe = {}
+    for element in flat_recipe:
+        new_recipe[element] = flat_recipe[element] * n
+    return new_recipe
 
 
 def make_grocery_list(flat_recipes):
@@ -104,10 +107,54 @@ def make_grocery_list(flat_recipes):
     should return:
         {'milk':3, 'chocolate': 1, 'sugar': 1}
     """
-    raise NotImplementedError
+    new_grocery_list = {}
+    for recipe in flat_recipes:
+        for element in recipe:
+            if element in new_grocery_list:
+                new_grocery_list[element] += recipe[element]
+            else:
+                new_grocery_list[element] = recipe[element]
+    return new_grocery_list
 
 
-def cheapest_flat_recipe(recipes, food_item):
+def cheapest_flat_recipe_helper(recipe_book, atomic_costs, food_item, fobiddens=None):
+    if fobiddens == None:
+        fobiddens = []
+    if food_item in fobiddens:
+        return False, 0, None
+    if food_item in atomic_costs:
+        return True, atomic_costs[food_item], {food_item: 1}
+    if food_item not in recipe_book:
+        return False, 0, None
+    recipes = recipe_book[food_item]
+    min_cost = -1
+    is_first = True
+    have_make = False
+    min_flat_recipe = None
+    for recipe in recipes:
+        current_cost = 0
+        can_make = True
+        flat_recipes = []
+        for element in recipe:
+            sub_can_make, sub_cost, sub_recipe = cheapest_flat_recipe_helper(
+                recipe_book, atomic_costs, element[0], fobiddens
+            )
+            if sub_can_make == False:
+                can_make = False
+                break
+            else:
+                current_cost += sub_cost * element[1]
+                sub_recipe = scale_recipe(sub_recipe, element[1])
+                flat_recipes.append(sub_recipe)
+        if can_make and (is_first or (current_cost < min_cost)):
+            is_first = False
+            min_cost = current_cost
+            min_flat_recipe = make_grocery_list(flat_recipes)
+            have_make = True
+    return have_make, min_cost, min_flat_recipe
+
+
+def cheapest_flat_recipe(recipes, food_item, fobiddens=None):
     """
     Given a recipes list and the name of a food item, return a dictionary
     (mapping atomic food items to quantities) representing the cheapest full
@@ -115,7 +162,15 @@ def cheapest_flat_recipe(recipes, food_item):
 
     Returns None if there is no possible recipe.
     """
-    raise NotImplementedError
+    recipe_book = make_recipe_book(recipes)
+    atomic_costs = make_atomic_costs(recipes)
+    have_make, min_cost, flat_recipe = cheapest_flat_recipe_helper(
+        recipe_book, atomic_costs, food_item, fobiddens
+    )
+    if have_make:
+        return flat_recipe
+    else:
+        return None
 
 
 def ingredient_mixes(flat_recipes):
